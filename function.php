d4b610874395fa1659950542bc3a39df4cd7a1aa
Minh Nhut Duong <nhutdm@gmail.com>
2014-03-15 18:42:45 +0700

    Use Composer Autoload to load library

--- function.php
+++ function.php
@@ -1,59 +1,64 @@
 <?php
-    session_start();
-    
-    include_once "config.php";
-    include_once "includes/facebook.php";
 
-    $user = null;
-    $liked = null;
-    
-    $facebook = new Facebook(array(
-        'appId'  => APPID,
-        'secret' => APPSECRET,
+session_start();
+
+require 'vendor/autoload.php';
+require 'config.php';
+
+$user = null;
+$liked = null;
+
+$facebook = new Facebook(
+    array(
+        'appId' => APP_ID,
+        'secret' => APP_SECRET,
         'cookie' => true
-    ));
+    )
+);
 
-    $user = $facebook->getUser();
-    
-    $loginUrl = $facebook->getLoginUrl(
-        array(
-            'scope'         => 'publish_stream,user_likes',
-            'redirect_uri'  => BASEURL
-        )
-    );
-    
-    if ($user) {
+$user = $facebook->getUser();
+
+$loginUrl = $facebook->getLoginUrl(
+    array(
+        'scope' => 'publish_stream, user_likes',
+        'redirect_uri' => BASE_URI
+    )
+);
+
+if ($user) {
+    try {
+        $likes = $facebook->api('/me/likes' . PAGE_ID);
+
+        if (!empty($likes['date'])) {
+            $liked = true;
+        } else {
+            $liked = false;
+        }
+    } catch (FacebookApiException $e) {
+        print_r($e);
+    }
+
+    if (isset($_GET['code'])) {
+        header('Location: ' . BASE_URI);
+        exit;
+    }
+
+    if (isset($_GET['checkin'])) {
         try {
-            $likes = $facebook->api('/me/likes/' . PAGEID);
-            if(!empty($likes['data']))
-                $liked = true;
-            else
-                $liked = false;
+            $status = $facebook->api("$user/feed", 'post',
+                array(
+                    'message' => MESSAGE,
+                    'place' => PAGE_ID
+                )
+            );
         } catch (FacebookApiException $e) {
             print_r($e);
         }
 
-        if (isset($_GET['code'])) {
-            header("Location: " . BASEURL);
-            exit;
-        }
-        
-        if (isset($_GET['checkin'])) {
-            try {
-                $status = $facebook->api("/$user/feed", 'post',
-                    array(
-                        'message'=> MESSAGE,
-                        'place' => PAGEID
-                    )
-                );
-            } catch (FacebookApiException $e) {
-                print_r($e);
-            }
-            if(!isset($_SESSION['shared']))
-                $_SESSION['shared'] = true;
-            header("Location: " . BASEURL);
-            exit;
-        }
+        if(!isset($_SESSION['shared']))
+            $_SESSION['shared'] = true;
+
+        header('Location: ' . BASE_URI);
+        exit;
     }
-    
-?>
+}
